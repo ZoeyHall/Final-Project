ed41e382f39b118e0b80ad0081c625b55b8e5196
Omotola Amodu <omotolaamodu15@gmail.com>
2022-08-04 20:17:14 -0500

    Worked on photos page. Allowed users to select photo from gallery and import it.

--- Final Project/photosViewController.swift
+++ Final Project/photosViewController.swift
@@ -4,26 +4,38 @@
 //
 //  Created by scholar on 8/3/22.
 //
+//, UIImagePickerControllerDelegate & UINavigationControllerDelegate
 
 import UIKit
 
-class photosViewController: UIViewController {
+class photosViewController:
+    UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageViewTwo: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+    }
+  
+    @IBAction func didTapButtonTwo(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+       print("\(info)")
+        picker.dismiss(animated: true, completion: nil)
+        //imageView.image = image
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
